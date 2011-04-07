373580ad3c55b010fa363cf829e18acd79c1d1ff
Erven Rohou <erven.rohou@inria.fr>
2011-04-07 15:15:06 +0000

    More on screens.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/tiptop/trunk@11 fd2ea14d-3e48-4a98-bac5-13ccbbe52f7f

--- screen.c
+++ screen.c
@@ -1,10 +1,18 @@
 #include <assert.h>
 #include <inttypes.h>
+
+#ifdef KERNEL31
+#include <linux/perf_counter.h>
+#else
+#include <linux/perf_event.h>
+#endif
+
 #include <stdlib.h>
 #include <string.h>
 
 #include "screen.h"
 
+static const int alloc_chunk = 10;
 
 static int num_screens = 0;
 
@@ -15,7 +23,11 @@ screen_t* new_screen(const char* name)
   num_screens++;
   the_screen->name = strdup(name);
   the_screen->num_counters = 0;
-  the_screen->counters = NULL;
+  the_screen->num_alloc_counters = alloc_chunk;
+  the_screen->counters = malloc(alloc_chunk * sizeof(counter_t));
+  the_screen->num_columns = 0;
+  the_screen->num_alloc_columns = alloc_chunk;
+  the_screen->columns = malloc(alloc_chunk * sizeof(counter_t));
   return the_screen;
 }
 
@@ -24,7 +36,13 @@ screen_t* new_screen(const char* name)
 int add_counter(screen_t* s, int32_t type, int64_t config)
 {
   int n = s->num_counters;
-  /* check max counter */
+  /* check max available hw counter */
+  if (n == s->num_alloc_counters) {
+    s->counters = realloc(s->counters, sizeof(counter_t) * (n + alloc_chunk));
+    n += alloc_chunk;
+    s->num_counters += alloc_chunk;
+  }
+  /* check if already in list ?? */
   s->counters[n].type = type;
   s->counters[n].config = config;
   s->num_counters++;
@@ -32,9 +50,27 @@ int add_counter(screen_t* s, int32_t type, int64_t config)
 }
 
 
+int add_column(screen_t* s)
+{
+}
+
+
+screen_t* default_screen()
+{
+  screen_t s = new_screen("default");
+  add_counter(s, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CPU_CYCLES);
+  add_counter(s, PERF_TYPE_HARDWARE, PERF_COUNT_HW_INSTRUCTIONS);
+  add_counter(s, PERF_TYPE_HARDWARE, PERF_COUNT_HW_CACHE_MISSES);
+  add_counter(s, PERF_TYPE_HARDWARE, PERF_COUNT_HW_BRANCH_MISSES);
+  /* add columns */
+}
+
+
 void delete_screen(screen_t* s)
 {
   assert(s);
   free(s->name);
+  free(s->counters);
+  free(s->columns);
   free(s);
 }
--- screen.h
+++ screen.h
@@ -11,11 +11,30 @@ typedef struct {
 
 
 typedef struct {
-  int   id;
-  char* name;
-  int   num_counters;
+  char* header;
+  char* format;  /* as in printf */
+  
+} column_t;
+
+
+typedef struct {
+  int     id;
+  char*   name;
+  int        num_counters;
+  int        num_alloc_counters;
   counter_t* counters;
+  int        num_columns;
+  int        num_alloc_columns;
+  column_t*  columns;
 } screen_t;
+
+screen_t* new_screen(const char* name);
+int add_counter(screen_t* s, int32_t type, int64_t config);
+int add_column(screen_t* s);
+screen_t* default_screen();
+
+void delete_screen(screen_t* s);
+
 
 
 #endif  /* _SCREEN_H */
