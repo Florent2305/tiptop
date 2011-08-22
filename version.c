6dac0e8a64671fa866366b61dba9d9c205057ab1
Erven Rohou <erven.rohou@inria.fr>
2011-08-22 14:28:14 +0000

    Text nit.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/tiptop/trunk@65 fd2ea14d-3e48-4a98-bac5-13ccbbe52f7f

--- version.c
+++ version.c
@@ -5,6 +5,6 @@
 
 void print_version()
 {
-  printf("tiptop was compiled on \"%s\" by host \"%s\".\n",
+  printf("tiptop was compiled on \"%s\" on host \"%s\".\n",
          compile_date, compile_host);
 }
