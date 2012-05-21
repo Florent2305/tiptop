84a444d5ac69e7e7fe7c8b4c04a33e92da4ded5a
Antoine Naudin <antoine.naudin@inria.fr>
2012-05-21 14:14:26 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/tiptop/trunk@163 fd2ea14d-3e48-4a98-bac5-13ccbbe52f7f

---
+++ src/config-parser.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <libxml/parser.h>
+#include <assert.h>
+
+
+typedef struct conf_screen conf_screen;
+typedef struct conf_counter conf_counter;
+typedef struct conf_view conf_view;
+
+struct conf_counter {
+  char* alias;
+  char* name;
+  int value;
+};
+
+
+struct conf_view {
+  char* header;
+  char* desc;
+  char* expr;
+  char* format;
+  conf_counter listC[512];
+  int nbC;
+};
+
+
+struct conf_screen {
+  char* name;
+  conf_view listV[512];
+  int nbV;
+};
+
+void option_update(xmlChar* name, xmlChar* val);
+void counter_update(xmlChar* alias, xmlChar* name, xmlChar* value);
+void view_update(xmlChar* header, xmlChar* format, xmlChar* desc, xmlChar* expr);
+void screen_update(xmlChar *name);
+
+
+int parse_doc(char* file, struct option* op);
+void parse_screen(xmlDocPtr doc, xmlNodePtr cur);
+void parse_counters(xmlDocPtr doc, xmlNodePtr cur);
+void parse_options(xmlDocPtr doc, xmlNodePtr cur);
